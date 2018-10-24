03bc01a57ef4b0a3caed141e70891e83948fbdca
Ran Gong <ran.gong@slalom.com>
2018-10-24 18:25:28 -0400

    add api call

--- blockon/src/components/LoginButtons/LoginButtons.js
+++ blockon/src/components/LoginButtons/LoginButtons.js
@@ -5,11 +5,25 @@ import './LoginButtons.css';
 
 
 class LoginButtons extends Component {
+    callApi(){
+        // Github fetch library : https://github.com/github/fetch
+        // Call the API page
+        fetch('http://localhost:4000')
+        .then((result) => {
+          // Get the result
+          // If we want text, call result.text()
+          return result.json();
+        }).then((jsonResult) => {
+          // Do something with the result
+          console.log(jsonResult);
+        })
+      }
 
     render = () => (
         <div className="buttonBg">
             <div className="buttonWrapper">
                 <Button.Group vertical>
+                    <Button onClick={() => this.callApi()}>Click here to call API</Button>
                     <Button className="loginButton" size='huge'>Physician</Button>
                     <Button className="loginButton" size='huge'>Pharmachist</Button>
                     <Button className="loginButton" size='huge'>Patient</Button>
