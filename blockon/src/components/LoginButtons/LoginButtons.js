d54a5ef298f2c2a6bdf79c035594211fb44dba54
Ran Gong <ran.gong@slalom.com>
2018-11-06 21:43:08 -0500

    Remove unnessary

--- blockon/src/components/LoginButtons/LoginButtons.js
+++ blockon/src/components/LoginButtons/LoginButtons.js
@@ -6,64 +6,12 @@ import './LoginButtons.css';
 
 
 class LoginButtons extends Component {
-    callApi(){
-        console.log("try to fetch api");
-        fetch('http://localhost:3001/queryallcars')
-        .then((result) => {
-          // Get the result
-          // If we want text, call result.text()
-          return result.json();
-        }).then((jsonResult) => {
-          // Do something with the result
-          console.log(jsonResult);
-        })
-      }
-
-    postPrescription(){
-        fetch('http://localhost:3001/PostPrescription/114',{
-            method:'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                "Name": "Drug1",
-                "CreateDate": "11/03/2018",
-                "Status":"Active",
-                "ControlledSubstance":"False",
-                "Schedule":"Schedule II",
-                "Dosage":"20mg",
-                "Brand":"Generic",
-                "LastDispenseDate":"N/A",
-               "NumberOfRefills":"3",
-                "Phamacy":"CVS in Boston"
-
-              })
-            
-        })
-        .then((response)=>response.text())
-        .then((responseText)=>{
-            console.log(responseText);
-        })
-        .catch((error)=>{
-            console.error(error);
-        });
-
-    }
-      
-
-
-    
-
     render = () => (
         <div>
             <div className="buttonBg"></div>
             <Image src={headerLogo} alt="logo" className="loginLogo"/>
             <div className="buttonWrapper">
                 <Button.Group vertical>
-
-                    <Button onClick={() => this.postPrescription()}>Click here to POST API</Button>
-                    <Button onClick={() => this.callApi()}>Click here to call API</Button>
                     <Link className="loginButton"  to="/auth/physician"><Button size='huge'>Physician</Button></Link>
                     <Link className="loginButton"  to="/auth/pharmacist"><Button size='huge'>Pharmacist</Button></Link>
                     <Link className="loginButton" to="/auth/patient"><Button size='huge'>Patient</Button></Link>
